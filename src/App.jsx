c311894fe48d9c3b27644dbe8a61919f5c0ad4fa
Mehrand3v <mehrand3v@gmail.com>
2025-03-17 09:55:49 +1100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from './components/ThemeProvider'
+import Home from '@/pages/Home'
+
+function App() {
+
+  return (
+     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Home />
+    </ThemeProvider>
+
+
+  )
+}
+
+export default App
